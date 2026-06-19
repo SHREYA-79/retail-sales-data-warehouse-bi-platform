bf9866c8450dc6ba7d5314be5317ba5d97db73d1
Shreya Reddy Chintha <shreya.61219@gmail.com>
2026-06-19 10:26:47 +0530

    Create initial setup script for Data Warehouse
    This script sets up the Data Warehouse by creating the database, schema, fact tables, and dimension tables.

---
+++ SQL/01_data_warehouse_setup.sql
@@ -0,0 +1,13 @@
+/*
+=========================================================
+01_data_warehouse_setup.sql
+=========================================================
+
+Contains:
+- Database Creation
+- Schema Creation
+- Fact Table Creation
+- Dimension Table Creation
+
+Database: DataWarehouseAnalytics
+*/
